c9d47adf71157990e73e737adcd6ce58907f7cc8
Ian Duggan <ian@ianduggan.net>
2023-06-15 20:11:03 -0700

    basic home page template working

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import customTheme from "../lib/theme";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <ChakraProvider theme={customTheme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
+}
+
+export default MyApp;
